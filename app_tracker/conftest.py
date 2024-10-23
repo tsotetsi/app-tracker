8f878476c818fc141bd53c8719b6780337d3ba77
Thapelo Tsotetsi <thapelo.tsotetsi@tsolo.io>
2024-10-23 17:32:09 +0200

    Init project. Create tracker app. Setup DB.

---
+++ app_tracker/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from app_tracker.users.models import User
+from app_tracker.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
